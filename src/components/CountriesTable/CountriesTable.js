ba7fb1badfc79a026a181edc4bbc4037a8cdbffa
SooditK <sooditkumarabc@gmail.com>
2021-06-21 18:29:58 +0530

    CountriesTable & module.css Updated

--- src/components/CountriesTable/CountriesTable.js
+++ src/components/CountriesTable/CountriesTable.js
@@ -1,22 +1,41 @@
+import { KeyboardArrowDownRounded } from "@material-ui/icons";
 import styles from "./CountriesTable.module.css";
 
+const orderBy = (countries, direction) => {
+  if (direction === "asc") {
+    return [...countries].sort((a, b) =>
+      a.population > b.population ? 1 : -1
+    );
+  }
+  if (direction === "desc") {
+    return [...countries].sort((a, b) =>
+      a.population > b.population ? -1 : 1
+    );
+  }
+  return countries;
+};
+
 const CountriesTable = ({ countries }) => {
+  const orderedCountries = orderBy(countries, "desc");
   return (
     <div>
       <div className={styles.heading}>
         <button className={styles.heading_name}>
           <div>Name</div>
+          <KeyboardArrowDownRounded />
         </button>
         <button className={styles.heading_population}>
           <div>Population</div>
+          <KeyboardArrowDownRounded />
         </button>
       </div>
-      {countries.map((country) => {
-        <div className={styles.row}>
-          <div className={styles.name}>Hello</div>
+      {orderedCountries.map((country, index) => (
+        <div className={styles.row} key={index}>
+          <div className={styles.name}>{country.name}</div>
           <div className={styles.population}>{country.population}</div>
-        </div>;
-      })}
+        </div>
+      ))}
+      {console.log(countries)}
     </div>
   );
 };
